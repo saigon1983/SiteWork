ffec300a391d3d2eefd93975c02635e93cfcab90
saigon1983 <rodionov.homeappliance@gmail.com>
2017-05-02 17:58:50 +0300

    Работы завершены. Запущена вторая версия

--- Libs/classSKUList.py
+++ Libs/classSKUList.py
@@ -21,6 +21,7 @@ class SKUList:
         else:                           raise TypeError ('Wrong data passed to SKUList constructor!')
         self.counter = 0
         self.setupArticlesArray()
+        self.setType()
 # ========== Перегруженные методы ==========
     def __str__(self):
         # Метод строкового представления объекта списка товаров
@@ -49,6 +50,19 @@ class SKUList:
         self.articlesArray = []
         for sku in self.array:
             self.articlesArray.append(sku.article)
+    def setType(self):
+        # Метод утановки типа списка. Для гомогенных списков тип равен наименованию ТГ2, для остальных - Смешанный
+        if self.isHomogenous():
+            tg1type = self.array[0].TG['ТГ1']
+            tg2type = self.array[0].TG['ТГ2']
+            if tg2type == 'Подогреватели, вакууматоры' or self.types[1] == 'Панели домино':
+                print()
+                tg2type = ' '.join([X.capitalize() for X in tg2type.split()])
+            if tg1type == 'Встраиваемая техника':
+                tg2type += ' ВСТР'
+            self.types = (tg1type, tg2type)
+        else:
+            self.types = ('Неопределена', 'Смешанный')
 # ========== Методы изменения объекта ==========
     def append(self, item):
         # Метод добавления элемента в список. Элемент обязан быть либо экземпляром класса SKU, либо списком или кортежем
@@ -68,6 +82,7 @@ class SKUList:
         self.array += data
         for item in data:
             self.articlesArray.append(item.article)
+        self.setType()
     def remove(self, item):
         # Метод удаляет указанный элемент из списка
         try:
@@ -76,6 +91,7 @@ class SKUList:
             self.articlesArray.remove(self.articlesArray[index])
         except:
             raise ValueError ('No such item in SKUList object!')
+        self.setType()
     def removeAllByArticle(self, article):
         # Метод удаляет ВСЕ элементы списка, идетничные указанному элементу item
         try:
@@ -86,6 +102,7 @@ class SKUList:
                 self.array.remove(self.array[index])
         except:
             raise ValueError ('No SKU with article {} in SKUList object!'.format(article))
+        self.setType()
 # ========== Методы получения элементов и выборок ==========
     def getIndexByItem(self, item):
         # Метод возвращает индекс указанного SKU
@@ -163,6 +180,21 @@ class SKUList:
         for item in self.array:
             if item not in other: resultArray.append(item)
         return SKUList(resultArray)
+    def splitToHomogenous(self):
+        # Метод разделяет текущий список на несколько гомогенных (по ТГ2) списков и возвращает словарь списков
+        newArray = {}
+        newArray['Прочие'] = SKUList()
+        for item in self.array:
+            tg2 = item.TG['ТГ2']
+            if tg2:
+                if tg2 not in newArray.keys():
+                    newArray[tg2] = SKUList()
+                newArray[tg2].append(item)
+            else:
+                newArray['Прочие'].append(item)
+        for someList in newArray.values():
+            someList.setType()
+        return newArray
 # ========== Методы установки значений SKU ==========
     def setTG1(self, TG):
         # Метод устанавливает все элементам набора ТГ1 равную TG
@@ -187,9 +219,12 @@ class SKUList:
 # ========== Методы сохранения набора ==========
     def isHomogenous(self):
         # Метод возвращает True, если все его SKU принадлежат одной ТГ2
+        if not self.array: return False
+        tg1 = self.array[0].TG['ТГ1']
         tg2 = self.array[0].TG['ТГ2']
         for item in self.array:
-            if item.TG['ТГ2'] != tg2: return False
+            if item.TG['ТГ2']   != tg2: return False
+            elif item.TG['ТГ1'] != tg1: return False
         return True
     def saveToExcel(self, filename):
         HEADERS = ['Артикул','Тип прибора','Бренд','Модель','ТГ1','ТГ2','ТГ3','ТГ4','ТГ5','Цвет']
@@ -208,17 +243,21 @@ class SKUList:
             workSheet['H{}'.format(i+2)] = self.array[i].TG['ТГ4']
             workSheet['I{}'.format(i+2)] = self.array[i].TG['ТГ5']
             workSheet['J{}'.format(i+2)] = self.array[i].color
-        if self.isHomogenous():
-            tradeGroup = self.array[0].TG['ТГ2']
+        if self.isHomogenous() and self.types[1] != 'Смешанный':
+            PARASECTIONS = ('ОПИСАНИЕ ПРИБОРА','ОПИСАНИЕ','ОСНОВНЫЕ ХАРАКТЕРИСТИКИ','БЕЗОПАСНОСТЬ','ФУНКЦИОНАЛ', 'МОРОЗИЛЬНАЯ КАМЕРА')
             groupsFile = ConfigObj('DataFolder\\groupsParameters.ini')
-            sections = groupsFile[tradeGroup]
-            PARAHEADERS = []
-            for parameter in sections.values():
-                PARAHEADERS += parameter
             i = 0
-            for name in PARAHEADERS:
-                workSheet['{}1'.format(openpyxl.utils.get_column_letter(i+len(HEADERS)+1))] = name
-                i += 1
+            for fieldGroup in PARASECTIONS:
+                if fieldGroup in groupsFile[self.types[1]]:
+                    fieldName = fieldGroup
+                    params = groupsFile[self.types[1]][fieldName]
+                    if type(params) == str:
+                        workSheet['{}1'.format(openpyxl.utils.get_column_letter(i+len(HEADERS)+1))] = params
+                        i += 1
+                    else:
+                        for name in groupsFile[self.types[1]][fieldName]:
+                            workSheet['{}1'.format(openpyxl.utils.get_column_letter(i+len(HEADERS)+1))] = name
+                            i += 1
         workBook.save('Output\\' + filename + '.xlsx')
 # ========== Методы класса ==========
     @classmethod
--- main.py
+++ main.py
@@ -5,20 +5,19 @@ from Libs.classSKUList import *
 from string import ascii_letters
 from Libs.constructTable_parameters import constructParametersTable
 
-data1 = parseData('DataFolder\\no_text.txt')
-#data2 = parseData('DataFolder\\no_properties.txt')
+#data1 = parseData('DataFolder\\no_text.txt')
+data2 = parseData('DataFolder\\no_properties.txt')
 SKUS2 = SKUList()
-for item in data1[0]:
+for item in data2[0]:
     SKUS2.append(SKU.fromData(item))
 newBase = Database.loadDatabase()
 #newBase = Database.construct()
 #newBase.saveDatabase()
 
-i = 0
-j = 0
 for item in SKUS2:
     item.fillFromDatabase(newBase)
-for item in SKUS2:
-    item.getInfo()
-print(i)
-print(j)
+homogenList = SKUS2.splitToHomogenous()
+for skuList in homogenList.values():
+    #skuList.saveToExcel(skuList.types[1])
+    if skuList.types[0]:
+        print(skuList.types[1])
